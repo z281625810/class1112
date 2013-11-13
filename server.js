d07e93afafac02177419f22dc24d811ed2f00412
z281625810 <sunfeng16@yahoo.com>
2013-11-12 20:11:24 -0500

    dsd

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿var http = require('http');
+var s = http.createServer(function(req,res)
+{
+	
+   res.writeHead(200,{'content-type':'text/plain'});
+
+   res.end('Hello World\n');
+  
+});
+
+s.listen(8000);
